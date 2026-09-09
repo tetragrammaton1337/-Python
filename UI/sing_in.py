d7eecba14352194181438e486ecd64f18d0707eb
52 <kirillbor43@gmail.com>
2026-09-09 19:15:51 +0400

    дизайн главного окна

--- UI/sing_in.py
+++ UI/sing_in.py
@@ -5,8 +5,8 @@ import sys
 app = QtWidgets.QApplication(sys.argv) #инициализируем приложение
 window = QtWidgets.QWidget() # создаём окно
 window.setWindowTitle("Вход") # заголовок окна
-window.resize(900, 500) #размер
+window.resize(400, 300) #размер
 
+btn = QtWidgets.QPushButton("Close")
 
-window.show() # отображение океа
-sys.exit(app.exec_()) # чтобы окно не закрывалось
+window.show()
